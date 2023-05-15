56323d962cd052446431a9faa38c09ab51c83dd2
Shmakov Alexander <schmakov8@gmail.com>
2023-05-15 10:51:25 +0500

    правки

--- ntgspi-master/server/main.js
+++ ntgspi-master/server/main.js
@@ -41,7 +41,7 @@ fastify.register(require('fastify-autoload'), {
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3001 }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
--- ntgspi-master/server/migrations/300_create-courses-table.js
+++ ntgspi-master/server/migrations/300_create-courses-table.js
@@ -59,7 +59,7 @@ exports.up = pgm => {
         ifNotExists: true,
         comment: 'Таблица, где хранится информация о пользователе'
     });
-    pgm.sql(`insert into courses (coursename, coursehours, coursetarget, courseprice, courseprogrammtarget, coursetypes_coursetypeid, teachers_teacherid, meta, img)
+    pgm.sql(`insert into courses (coursename, coursehours, coursetarget, courseprice, courseprogrammtarget, coursetypes_coursetypeid, teacherid, meta, img)
 values  ('Подготовка к ЕГЭ по профильной математике', 1, '1', 1.00, '1', 1, 1, '{"rating": 4.5, "newPrice": 3200, "oldPrice": 8500, "ratingCount": 48, "lessonsCount": 25, "courseBegin": "09.06.2023", "courseEnd": "09.09.2023"}', './img/course1.jpg'),
         ('das', 1, 'das', 0.00, '0', 1, 1, '{"rating": "213", "newPrice": "312", "oldPrice": "321", "ratingCount": "312", "lessonsCount": "123", "courseBegin": "09.06.2023", "courseEnd": "09.09.2023"}', './img/map.png');`)
 }
